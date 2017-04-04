8c0d6d438f1ac04c1fca88218bef169cc7769682
RoxanaRRS <rodriguezroxana116@gmail.com>
2017-04-04 09:19:04 -0600

    base

---
+++ app/src/main/java/com/example/roxana/probando/api/Api.java
@@ -0,0 +1,10 @@
+package com.example.roxana.probando.api;
+
+/**
+ * Created by Roxana on 4/4/2017.
+ */
+
+public class Api {
+    
+
+}
---
+++ app/src/main/java/com/example/roxana/probando/api/ApiInterface.java
@@ -0,0 +1,8 @@
+package com.example.roxana.probando.api;
+
+/**
+ * Created by Roxana on 4/4/2017.
+ */
+
+public interface ApiInterface {
+}
